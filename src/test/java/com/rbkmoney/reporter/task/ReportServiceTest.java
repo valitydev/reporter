f2ae19faa17cdb0e20f7ae89ef83779a56031e03
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-04-11 17:19:45 +0300

    Fix for great justice

--- src/test/java/com/rbkmoney/reporter/task/ReportServiceTest.java
+++ src/test/java/com/rbkmoney/reporter/task/ReportServiceTest.java
@@ -107,7 +107,6 @@ public class ReportServiceTest extends AbstractIntegrationTest {
         for (FileMeta fileMeta : reportFiles) {
             URL url = reportService.generatePresignedUrl(fileMeta.getFileId(), LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC));
             assertNotNull(url);
-
             try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
                 try (InputStream inputStream = url.openStream()) {
                     Streams.copy(inputStream, outputStream, true);
