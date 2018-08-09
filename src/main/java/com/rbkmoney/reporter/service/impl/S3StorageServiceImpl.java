20957f26120c9f21f869d196d83bf339f86892ca
Pavel Popov <tolkonepiu@users.noreply.github.com>
2018-08-09 15:02:42 +0300

    Optimize storage service for big files (#38)
    * Optimize storage service for big files
    
    * Swap methods
    
    * MD2 -> MD5

--- src/main/java/com/rbkmoney/reporter/service/impl/S3StorageServiceImpl.java
+++ src/main/java/com/rbkmoney/reporter/service/impl/S3StorageServiceImpl.java
@@ -70,10 +70,40 @@ public class S3StorageServiceImpl implements StorageService {
 
     @Override
     public FileMeta saveFile(Path file) throws FileStorageException {
+        String filename = file.getFileName().toString();
+        log.info("Trying to upload file to storage, filename='{}', bucketId='{}'", filename, bucketName);
+
         try {
-            return saveFile(file.getFileName().toString(), Files.readAllBytes(file));
-        } catch (IOException ex) {
-            throw new FileStorageException(String.format("Failed to save path in storage, filename='%s', bucketId='%s'", file.getFileName().toString(), bucketName), ex);
+            String fileId;
+            do {
+                fileId = UUID.randomUUID().toString();
+            } while (storageClient.doesObjectExist(bucketName, fileId));
+
+            FileMeta fileMeta = createFileMeta(
+                    fileId,
+                    bucketName,
+                    filename,
+                    DigestUtils.md5Hex(Files.newInputStream(file)),
+                    DigestUtils.sha256Hex(Files.newInputStream(file))
+            );
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentDisposition("attachment;filename=" + filename);
+            Upload upload = transferManager.upload(
+                    new PutObjectRequest(bucketName, fileId, Files.newInputStream(file), objectMetadata)
+            );
+            try {
+                upload.waitForUploadResult();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            log.info("File have been successfully uploaded, fileId='{}', bucketId='{}', filename='{}', md5='{}', sha256='{}'",
+                    fileMeta.getFileId(), fileMeta.getBucketId(), fileMeta.getFilename(), fileMeta.getMd5(), fileMeta.getSha256());
+
+            return fileMeta;
+
+        } catch (IOException | AmazonClientException ex) {
+            throw new FileStorageException(String.format("Failed to upload file to storage, filename='%s', bucketId='%s'", filename, bucketName), ex);
         }
     }
 
@@ -97,7 +127,6 @@ public class S3StorageServiceImpl implements StorageService {
 
             ObjectMetadata objectMetadata = new ObjectMetadata();
             objectMetadata.setContentDisposition("attachment;filename=" + filename);
-            objectMetadata.setContentLength(bytes.length);
             Upload upload = transferManager.upload(
                     new PutObjectRequest(bucketName, fileId, new ByteArrayInputStream(bytes), objectMetadata)
             );
