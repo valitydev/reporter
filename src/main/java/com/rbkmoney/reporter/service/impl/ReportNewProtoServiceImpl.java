9112b4e2dcf63e458253ff483448805395b709f1
Pavel Popov <tolkonepiu@users.noreply.github.com>
2020-05-15 16:31:44 +0300

    Yet another fix with shopId (#107)
    * Yet antoher fix with shopId
    
    * Remove unused import

--- src/main/java/com/rbkmoney/reporter/service/impl/ReportNewProtoServiceImpl.java
+++ src/main/java/com/rbkmoney/reporter/service/impl/ReportNewProtoServiceImpl.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -60,7 +60,7 @@ public class ReportNewProtoServiceImpl implements ReportNewProtoService {
         try {
             return reportDao.getReportsWithToken(
                     partyId,
-                    Objects.requireNonNullElse(Collections.singletonList(shopId), Collections.emptyList()),
+                    Optional.ofNullable(shopId).map(Collections::singletonList).orElse(Collections.emptyList()),
                     reportTypes,
                     LocalDateTime.ofInstant(fromTime, ZoneOffset.UTC),
                     LocalDateTime.ofInstant(toTime, ZoneOffset.UTC),
