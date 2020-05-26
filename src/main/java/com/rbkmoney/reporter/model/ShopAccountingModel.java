a4e926c74f242440bc3b6c9416045b136bac7154
Pavel Popov <tolkonepiu@users.noreply.github.com>
2020-05-26 17:48:52 +0300

    BJ-921: Add chargebacks (#111)
    * BJ-921: Add chargebacks
    
    * Add default constructor in ShopAccountingModel
    
    * Bump easyway

--- src/main/java/com/rbkmoney/reporter/model/ShopAccountingModel.java
+++ src/main/java/com/rbkmoney/reporter/model/ShopAccountingModel.java
@@ -1,12 +1,15 @@
 package com.rbkmoney.reporter.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.Objects;
 
-/**
- * Created by tolkonepiu on 17/07/2017.
- */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShopAccountingModel {
 
     @NotNull
@@ -32,111 +35,17 @@ public class ShopAccountingModel {
     @Min(0)
     private long fundsRefunded;
 
-    public ShopAccountingModel(String merchantId, String shopId, String currencyCode) {
+    @Min(0)
+    private long fundsReturned;
+
+    public ShopAccountingModel(@NotNull String merchantId, @NotNull String shopId, @NotNull String currencyCode) {
         this.merchantId = merchantId;
         this.shopId = shopId;
         this.currencyCode = currencyCode;
-    }
-
-    public String getMerchantId() {
-        return merchantId;
-    }
-
-    public void setMerchantId(String merchantId) {
-        this.merchantId = merchantId;
-    }
-
-    public String getShopId() {
-        return shopId;
-    }
-
-    public void setShopId(String shopId) {
-        this.shopId = shopId;
-    }
-
-    public String getCurrencyCode() {
-        return currencyCode;
-    }
-
-    public void setCurrencyCode(String currencyCode) {
-        this.currencyCode = currencyCode;
-    }
-
-    public long getFundsAcquired() {
-        return fundsAcquired;
-    }
-
-    public void setFundsAcquired(long fundsAcquired) {
-        this.fundsAcquired = fundsAcquired;
-    }
-
-    public long getFeeCharged() {
-        return feeCharged;
-    }
-
-    public void setFeeCharged(long feeCharged) {
-        this.feeCharged = feeCharged;
-    }
-
-    public long getFundsPaidOut() {
-        return fundsPaidOut;
-    }
-
-    public void setFundsPaidOut(long fundsPaidOut) {
-        this.fundsPaidOut = fundsPaidOut;
-    }
-
-    public long getFundsAdjusted() {
-        return fundsAdjusted;
-    }
-
-    public void setFundsAdjusted(long fundsAdjusted) {
-        this.fundsAdjusted = fundsAdjusted;
-    }
-
-    public long getFundsRefunded() {
-        return fundsRefunded;
-    }
-
-    public void setFundsRefunded(long fundsRefunded) {
-        this.fundsRefunded = fundsRefunded;
     }
 
     public long getAvailableFunds() {
-        return fundsAcquired + fundsAdjusted - feeCharged - fundsPaidOut - fundsRefunded;
+        return fundsAcquired + fundsAdjusted - feeCharged - fundsPaidOut - fundsRefunded - fundsReturned;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ShopAccountingModel that = (ShopAccountingModel) o;
-        return fundsAcquired == that.fundsAcquired &&
-                feeCharged == that.feeCharged &&
-                fundsAdjusted == that.fundsAdjusted &&
-                fundsPaidOut == that.fundsPaidOut &&
-                fundsRefunded == that.fundsRefunded &&
-                Objects.equals(merchantId, that.merchantId) &&
-                Objects.equals(shopId, that.shopId) &&
-                Objects.equals(currencyCode, that.currencyCode);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(merchantId, shopId, currencyCode, fundsAcquired, feeCharged, fundsAdjusted, fundsPaidOut, fundsRefunded);
-    }
-
-    @Override
-    public String toString() {
-        return "ShopAccountingModel{" +
-                "merchantId='" + merchantId + '\'' +
-                ", shopId='" + shopId + '\'' +
-                ", currencyCode='" + currencyCode + '\'' +
-                ", fundsAcquired=" + fundsAcquired +
-                ", feeCharged=" + feeCharged +
-                ", fundsAdjusted=" + fundsAdjusted +
-                ", fundsPaidOut=" + fundsPaidOut +
-                ", fundsRefunded=" + fundsRefunded +
-                '}';
-    }
 }
